5f0acbc7e40bb96f47c16519356d5d34fa052fc9
Tejakatkam <tejakatkam2005@gmail.com>
2026-02-16 22:33:11 +0530

    Fix admin report event field from title to name

--- eventpass_backend/settings.py
+++ eventpass_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m$hkg%5ra4s2#=rp&wh*%b9db)lkul$qdedn*r4w#un35lq6l7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
